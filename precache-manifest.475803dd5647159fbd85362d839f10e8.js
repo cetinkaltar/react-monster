30af79591bd7fd1b523d61a4e27e08d7c58b2006
CetinKaltar <cetin.kaltar@bankish.eu>
2020-11-06 00:31:05 +0200

    Updates

---
+++ precache-manifest.475803dd5647159fbd85362d839f10e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86d2ae4febe7182fc55223473a58742",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "57bb15cd1c2c69021b01",
+    "url": "/react-monster/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/react-monster/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-monster/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57bb15cd1c2c69021b01",
+    "url": "/react-monster/static/js/main.2740c6ac.chunk.js"
+  },
+  {
+    "revision": "bb3f04e8bde7a5ae2bc9",
+    "url": "/react-monster/static/js/runtime-main.4e3149a6.js"
+  }
+]);
